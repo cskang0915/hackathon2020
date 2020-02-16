a2df545dc3dbae55a0b7b9d2f335de4d9913d912
Taylor K <taylorkmc@gmail.com>
2020-02-15 17:05:05 -0800

    Remove unnessecary files

--- emergency_detection/test_audio_file.py
+++ emergency_detection/test_audio_file.py
@@ -11,18 +11,4 @@ scaler = joblib.load(scaler_filename)
 
 classes = predict_probability(y, scaler)
 
-fig = plt.figure(figsize=(8,4))
-ax = fig.add_subplot(111)
-ax.plot(classes, c='b', linewidth = 3.0, alpha=0.5)
-scale_x = len(classes)/10.0
-ticks_x = ticker.FuncFormatter(lambda x, pos: '%.2f'%(x/scale_x))
-ax.xaxis.set_major_formatter(ticks_x)
-ax.set_yticks([0,1])
-ax.set_ylim([-0.1, 1.1])
-ax.set_xlabel("time (s)")
-ax.set_ylabel("Em signal presence")
-plt.grid('on')
-plt.tight_layout()
-plt.savefig('Em_probability.png')
-plt.show()
 
