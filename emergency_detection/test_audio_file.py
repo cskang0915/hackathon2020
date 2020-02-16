e3a8a72bcd0b79e3ddb6343cec8e6d076315e0fa
Taylor K <taylorkmc@gmail.com>
2020-02-15 21:09:31 -0800

    New file

--- emergency_detection/test_audio_file.py
+++ emergency_detection/test_audio_file.py
@@ -1,14 +1,23 @@
 # Test whether an Emergency signal is present in an audio sample
 from em_detection import *
+from time import sleep
 
 # Read the test file
-test_file = '../audio/sirena_ambulanza.wav'
-y, sr = librosa.load(test_file, sr=8000)
 
-# Load the scaler obtained from the train data
-scaler_filename = "scaler.save"
-scaler = joblib.load(scaler_filename)
+def run_detection(test_file):
+    y, sr = librosa.load(test_file, sr=8000)
 
-classes = predict_probability(y, scaler)
+    # Load the scaler obtained from the train data
+    scaler_filename = "scaler.save"
+    scaler = joblib.load(scaler_filename)
 
+    classes = predict_probability(y, scaler)
+    return classes
 
+if __name__ == "__main__":
+    print(run_detection("../audio/sirena-ambulanza.wav"))
+#     while True: 
+#         if 
+#             run_detection()
+#         else:
+#             sleep(5)
---
+++ run_detection.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd emergency_detection
+python test_audio_file.py
